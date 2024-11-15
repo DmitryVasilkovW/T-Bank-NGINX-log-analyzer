b71f3ceb8cda9c2ce5bf772bdd474b941cea3edb
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-11-15 20:27:10 +0300

    refactor: add functions for markdown rander

--- src/main/java/backend/academy/log/analyzer/service/render/impl/MarkdownReportRanderImpl.java
+++ src/main/java/backend/academy/log/analyzer/service/render/impl/MarkdownReportRanderImpl.java
@@ -6,6 +6,7 @@ import backend.academy.log.analyzer.service.render.http.response.decoder.HttpSta
 import java.util.Map;
 
 public class MarkdownReportRanderImpl implements ReportRander {
+    StringBuilder sb;
     private static final String SETTINGS_TITLE = "#### analyzer settings\n\n";
     private static final String GENERAL_INFO_TITLE = "#### General information \n\n";
     private static final String RESOURCES_REQUESTED_TITLE = "\n#### Resources requested\n\n";
@@ -27,35 +28,60 @@ public class MarkdownReportRanderImpl implements ReportRander {
     private static final String RESOURCES_ROW_FORMAT = "| %s | %d |\n";
     private static final String RESPONSE_CODES_ROW_FORMAT = "| %d | %s | %d |\n";
 
-    private static final String FILTRATION_NO = "no filtration";
+    private static final String FILTRATION_NO = "No filtration";
+    private static final String START_TIME_NAME = "Start time";
+    private static final String END_TIME_NAME = "End time";
+    private static final String FILTRATION_NAME = "Filtration";
+    private static final String NUMBER_OF_REQUESTS_NAME = "Number of requests";
+    private static final String AVERAGE_RESPONSE_SIZE_NAME = "Average response size";
+    private static final String P_RESPONSE_SIZE_NAME = "95p response size";
+    private static final String DIMENSIONALITY = "b";
+    private static final String UNKNOWN_CODE = "Unknown code";
 
     @Override
     public String randerReportAsString(Report report) {
-        var sb = new StringBuilder();
+        sb = new StringBuilder();
+        renderSettingsTable(report);
+        renderGeneralInfoTable(report);
+        renderResourcesTable(report);
+        renderRequestsCodeTable(report);
+
+        return sb.toString();
+    }
+
+    private void renderSettingsTable(Report report) {
         sb.append(SETTINGS_TITLE);
         sb.append(SETTINGS_TABLE_HEADER);
         sb.append(TABLE_SEPARATOR_FOR_PARAMETERS_AND_METRICS);
-        sb.append(String.format(GENERAL_INFO_ROW_AND_SETTINGS_ROW_FORMAT, "start time", report.dateFrom()));
-        sb.append(String.format(GENERAL_INFO_ROW_AND_SETTINGS_ROW_FORMAT, "End time", report.dateTo()));
-        sb.append(String.format(GENERAL_INFO_ROW_AND_SETTINGS_ROW_FORMAT, "filtration", FILTRATION_NO));
+        sb.append(String.format(GENERAL_INFO_ROW_AND_SETTINGS_ROW_FORMAT, START_TIME_NAME, report.dateFrom()));
+        sb.append(String.format(GENERAL_INFO_ROW_AND_SETTINGS_ROW_FORMAT, END_TIME_NAME, report.dateTo()));
+        sb.append(String.format(GENERAL_INFO_ROW_AND_SETTINGS_ROW_FORMAT, FILTRATION_NAME, FILTRATION_NO));
+    }
 
+    private void renderGeneralInfoTable(Report report) {
         sb.append(GENERAL_INFO_TITLE);
         sb.append(GENERAL_INFO_TABLE_HEADER);
         sb.append(TABLE_SEPARATOR_FOR_PARAMETERS_AND_METRICS);
         sb.append(String.format(
-            GENERAL_INFO_ROW_AND_SETTINGS_ROW_FORMAT, "Number of requests", report.totalRequests()));
+            GENERAL_INFO_ROW_AND_SETTINGS_ROW_FORMAT, NUMBER_OF_REQUESTS_NAME, report.totalRequests()));
         sb.append(String.format(
-            GENERAL_INFO_ROW_AND_SETTINGS_ROW_FORMAT, "Average response size", report.averageResponseSize() + "b"));
+            GENERAL_INFO_ROW_AND_SETTINGS_ROW_FORMAT, AVERAGE_RESPONSE_SIZE_NAME, report.averageResponseSize()
+                + DIMENSIONALITY));
         sb.append(String.format(
-            GENERAL_INFO_ROW_AND_SETTINGS_ROW_FORMAT, "95p response size", report.percentile95ResponseSize() + "b"));
+            GENERAL_INFO_ROW_AND_SETTINGS_ROW_FORMAT, P_RESPONSE_SIZE_NAME, report.percentile95ResponseSize()
+                + DIMENSIONALITY));
+    }
 
+    private void renderResourcesTable(Report report) {
         sb.append(RESOURCES_REQUESTED_TITLE);
         sb.append(RESOURCES_TABLE_HEADER);
         sb.append(TABLE_SEPARATOR_FOR_RESOURCES);
         for (Map.Entry<String, Long> entry : report.resourceCount().entrySet()) {
             sb.append(String.format(RESOURCES_ROW_FORMAT, entry.getKey(), entry.getValue()));
         }
+    }
 
+    private void renderRequestsCodeTable(Report report) {
         sb.append(RESPONSE_CODES_TITLE);
         sb.append(RESPONSE_CODES_TABLE_HEADER);
         sb.append(TABLE_SEPARATOR_FOR_RESPONSE_CODES);
@@ -64,9 +90,7 @@ public class MarkdownReportRanderImpl implements ReportRander {
             int code = entry.getKey();
             String description = HttpStatusDescription.getStatusDescription(code);
             sb.append(String.format(RESPONSE_CODES_ROW_FORMAT, code,
-                description != null ? description : "Unknown", entry.getValue()));
+                description != null ? description : UNKNOWN_CODE, entry.getValue()));
         }
-
-        return sb.toString();
     }
 }
