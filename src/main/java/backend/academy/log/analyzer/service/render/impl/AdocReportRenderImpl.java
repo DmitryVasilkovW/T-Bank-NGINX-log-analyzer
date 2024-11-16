424a79683106f2507cd924d330f342501b1c58fe
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-11-17 02:31:54 +0300

    fix: duplicate code

--- src/main/java/backend/academy/log/analyzer/service/render/impl/AdocReportRenderImpl.java
+++ src/main/java/backend/academy/log/analyzer/service/render/impl/AdocReportRenderImpl.java
@@ -3,29 +3,16 @@ package backend.academy.log.analyzer.service.render.impl;
 import backend.academy.log.analyzer.model.Pair;
 import backend.academy.log.analyzer.model.Report;
 import backend.academy.log.analyzer.service.render.ReportRender;
-import backend.academy.log.analyzer.service.render.http.response.decoder.HttpStatusDescription;
 import java.util.List;
-import static backend.academy.log.analyzer.service.render.common.constant.CommonConstant.AVERAGE_RESPONSE_SIZE_NAME;
-import static backend.academy.log.analyzer.service.render.common.constant.CommonConstant.DIMENSIONALITY;
-import static backend.academy.log.analyzer.service.render.common.constant.CommonConstant.END_TIME_NAME;
-import static backend.academy.log.analyzer.service.render.common.constant.CommonConstant.END_TIME_NO;
-import static backend.academy.log.analyzer.service.render.common.constant.CommonConstant.FILTRATION_NAME;
-import static backend.academy.log.analyzer.service.render.common.constant.CommonConstant.NUMBER_OF_REQUESTS_NAME;
-import static backend.academy.log.analyzer.service.render.common.constant.CommonConstant.PATH_NAME;
-import static backend.academy.log.analyzer.service.render.common.constant.CommonConstant.P_RESPONSE_SIZE_NAME;
-import static backend.academy.log.analyzer.service.render.common.constant.CommonConstant.SOURCES_NAME;
-import static backend.academy.log.analyzer.service.render.common.constant.CommonConstant.START_TIME_NAME;
-import static backend.academy.log.analyzer.service.render.common.constant.CommonConstant.START_TIME_NO;
-import static backend.academy.log.analyzer.service.render.common.constant.CommonConstant.UNKNOWN_CODE;
 import static backend.academy.log.analyzer.service.render.common.tools.MapSorter.getSortedPairs;
-import static backend.academy.log.analyzer.service.render.common.tools.StringMapper.getDataAsString;
-import static backend.academy.log.analyzer.service.render.common.tools.StringMapper.getFiltrationAsString;
-import static backend.academy.log.analyzer.service.render.common.tools.StringMapper.getSourcesAsString;
+import static backend.academy.log.analyzer.service.render.common.tools.RenderTables.renderGeneralInfoTable;
+import static backend.academy.log.analyzer.service.render.common.tools.RenderTables.renderRequestsCodeTable;
+import static backend.academy.log.analyzer.service.render.common.tools.RenderTables.renderSettingsTable;
+import static backend.academy.log.analyzer.service.render.common.tools.RenderTables.renderTopResponseCodesTable;
 import static backend.academy.log.analyzer.service.render.common.tools.TopParametersRender.renderTopParameterTable;
 
 public class AdocReportRenderImpl implements ReportRender {
     StringBuilder sb;
-    List<Pair<Integer, Long>> responseCodes;
     List<Pair<String, Long>> resources;
     private static final Integer AMOUNT_FOR_TOP = 3;
     private static final String GENERAL_TITLE = "== All information\n";
@@ -63,49 +50,25 @@ public class AdocReportRenderImpl implements ReportRender {
 
     private void renderGeneralInformation(Report report) {
         sb.append(GENERAL_TITLE);
-        renderSettingsTable(report);
-        renderGeneralInfoTable(report);
+        renderSettingsTable(sb, report, SETTINGS_TITLE, TABLE_SEPARATOR, SETTINGS_TABLE_HEADER, FORMAT_FOR_TWO_STRINGS);
+        sb.append(TABLE_SEPARATOR);
+        renderGeneralInfoTable(sb, report, GENERAL_INFO_TITLE, TABLE_SEPARATOR, GENERAL_INFO_TABLE_HEADER,
+            FORMAT_FOR_TWO_STRINGS);
+        sb.append(TABLE_SEPARATOR);
         renderResourcesTable(report);
-        renderRequestsCodeTable(report);
+        renderRequestsCodeTable(sb, report, RESPONSE_CODES_TITLE, TABLE_SEPARATOR, RESPONSE_CODES_TABLE_HEADER,
+            FORMAT_FOR_TWOINTEGERS_AND_ONE_STRING);
+        sb.append(TABLE_SEPARATOR);
     }
 
     private void renderCompilations(Report report) {
         sb.append(COMPILATIONS_TITLE);
         renderTopRequestedResourcesTable(report);
-        renderTopResponseCodesTable(report);
+        renderTopResponseCodesTable(sb, report, TOP_OF_RESPONSE_CODES_TITLE, TABLE_SEPARATOR,
+            RESPONSE_CODES_TABLE_HEADER, FORMAT_FOR_TWOINTEGERS_AND_ONE_STRING, AMOUNT_FOR_TOP);
+        sb.append(TABLE_SEPARATOR);
         renderTopIpAddressesTable(report);
         renderTopUserAgentsTable(report);
-    }
-
-    private void renderSettingsTable(Report report) {
-        sb.append(SETTINGS_TITLE);
-        sb.append(TABLE_SEPARATOR);
-        sb.append(SETTINGS_TABLE_HEADER);
-
-        String path = report.settingsReport().path();
-        String sources = getSourcesAsString(report.settingsReport().sources());
-        String dataFrom = getDataAsString(report.settingsReport().dateFrom(), START_TIME_NO);
-        String dataTo = getDataAsString(report.settingsReport().dateTo(), END_TIME_NO);
-        String filtration = getFiltrationAsString(report.settingsReport().filtration());
-
-        sb.append(String.format(FORMAT_FOR_TWO_STRINGS, PATH_NAME, path));
-        sb.append(String.format(FORMAT_FOR_TWO_STRINGS, SOURCES_NAME, sources));
-        sb.append(String.format(FORMAT_FOR_TWO_STRINGS, START_TIME_NAME, dataFrom));
-        sb.append(String.format(FORMAT_FOR_TWO_STRINGS, END_TIME_NAME, dataTo));
-        sb.append(String.format(FORMAT_FOR_TWO_STRINGS, FILTRATION_NAME, filtration));
-        sb.append(TABLE_SEPARATOR);
-    }
-
-    private void renderGeneralInfoTable(Report report) {
-        sb.append(GENERAL_INFO_TITLE);
-        sb.append(TABLE_SEPARATOR);
-        sb.append(GENERAL_INFO_TABLE_HEADER);
-        sb.append(String.format(FORMAT_FOR_TWO_STRINGS, NUMBER_OF_REQUESTS_NAME, report.totalRequests()));
-        sb.append(String.format(FORMAT_FOR_TWO_STRINGS, AVERAGE_RESPONSE_SIZE_NAME,
-            report.averageResponseSize() + DIMENSIONALITY));
-        sb.append(String.format(FORMAT_FOR_TWO_STRINGS, P_RESPONSE_SIZE_NAME,
-            report.percentile95ResponseSize() + DIMENSIONALITY));
-        sb.append(TABLE_SEPARATOR);
     }
 
     private void renderResourcesTable(Report report) {
@@ -123,24 +86,6 @@ public class AdocReportRenderImpl implements ReportRender {
         sb.append(TABLE_SEPARATOR);
     }
 
-    private void renderRequestsCodeTable(Report report) {
-        sb.append(RESPONSE_CODES_TITLE);
-        sb.append(TABLE_SEPARATOR);
-        sb.append(RESPONSE_CODES_TABLE_HEADER);
-
-        if (responseCodes == null) {
-            responseCodes = getSortedPairs(report.statusCount());
-        }
-
-        for (Pair<Integer, Long> entry : responseCodes) {
-            int code = entry.first();
-            String description = HttpStatusDescription.getStatusDescription(code);
-            sb.append(String.format(FORMAT_FOR_TWOINTEGERS_AND_ONE_STRING, code,
-                description != null ? description : UNKNOWN_CODE, entry.second()));
-        }
-        sb.append(TABLE_SEPARATOR);
-    }
-
     private void renderTopRequestedResourcesTable(Report report) {
         sb.append(TOP_OF_RESOURCES_TITLE);
         sb.append(TABLE_SEPARATOR);
@@ -151,26 +96,6 @@ public class AdocReportRenderImpl implements ReportRender {
         }
 
         renderTopParameterTable(sb, resources, AMOUNT_FOR_TOP, FORMAT_FOR_STRING_AND_INTEGER);
-        sb.append(TABLE_SEPARATOR);
-    }
-
-    private void renderTopResponseCodesTable(Report report) {
-        sb.append(TOP_OF_RESPONSE_CODES_TITLE);
-        sb.append(TABLE_SEPARATOR);
-        sb.append(RESPONSE_CODES_TABLE_HEADER);
-
-        if (responseCodes == null) {
-            responseCodes = getSortedPairs(report.statusCount());
-        }
-
-        int n = Math.min(AMOUNT_FOR_TOP, responseCodes.size());
-        for (int i = 0; i < n; i++) {
-            Pair<Integer, Long> entry = responseCodes.get(i);
-            int code = entry.first();
-            String description = HttpStatusDescription.getStatusDescription(code);
-            sb.append(String.format(FORMAT_FOR_TWOINTEGERS_AND_ONE_STRING, code,
-                description != null ? description : UNKNOWN_CODE, entry.second()));
-        }
         sb.append(TABLE_SEPARATOR);
     }
 
