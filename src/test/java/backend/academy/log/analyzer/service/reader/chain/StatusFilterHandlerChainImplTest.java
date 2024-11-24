18686f1952c97df785d28d7cc4d1b378872b1e88
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-11-24 15:15:00 +0300

    test: fix mocks

--- src/test/java/backend/academy/log/analyzer/service/reader/chain/StatusFilterHandlerChainImplTest.java
+++ src/test/java/backend/academy/log/analyzer/service/reader/chain/StatusFilterHandlerChainImplTest.java
@@ -1,89 +1,78 @@
 package backend.academy.log.analyzer.service.reader.chain;
 
 import backend.academy.log.analyzer.model.FilterRequest;
+import backend.academy.log.analyzer.model.LogRecord;
 import backend.academy.log.analyzer.model.Pair;
 import backend.academy.log.analyzer.service.reader.chain.impl.StatusFilterHandlerChainImpl;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class StatusFilterHandlerChainImplTest {
+    @Mock
+    private FilterRequest mockRequest;
+
+    @Mock
+    private LogRecord logRecord;
 
     @InjectMocks
     private StatusFilterHandlerChainImpl handler;
 
-    @Mock
-    private FilterRequest mockRequest;
-
-    @BeforeEach
-    void setUp() {
-        reset(mockRequest); // Сброс моков перед каждым тестом
-    }
-
     @Test
     void shouldReturnTrueWhenStatusMatches() {
-        // Arrange
         when(mockRequest.filtration()).thenReturn(Optional.of(new Pair<>("status", "200")));
+        when(mockRequest.logRecord()).thenReturn(logRecord);
         when(mockRequest.logRecord().statusCode()).thenReturn(200);
 
-        // Act
         boolean result = handler.handle(mockRequest);
 
-        // Assert
         assertTrue(result);
-        verify(mockRequest, times(1)).filtration();
+        verify(mockRequest, times(2)).filtration();
         verify(mockRequest.logRecord(), times(1)).statusCode();
     }
 
     @Test
     void shouldReturnFalseWhenStatusDoesNotMatch() {
-        // Arrange
         when(mockRequest.filtration()).thenReturn(Optional.of(new Pair<>("status", "404")));
+        when(mockRequest.logRecord()).thenReturn(logRecord);
         when(mockRequest.logRecord().statusCode()).thenReturn(200);
 
-        // Act
         boolean result = handler.handle(mockRequest);
 
-        // Assert
         assertFalse(result);
-        verify(mockRequest, times(1)).filtration();
+        verify(mockRequest, times(2)).filtration();
         verify(mockRequest.logRecord(), times(1)).statusCode();
     }
 
     @Test
     void shouldReturnFalseWhenFiltrationValueIsInvalid() {
-        // Arrange
         when(mockRequest.filtration()).thenReturn(Optional.of(new Pair<>("status", "invalid")));
+        when(mockRequest.logRecord()).thenReturn(logRecord);
         when(mockRequest.logRecord().statusCode()).thenReturn(200);
 
-        // Act
         boolean result = handler.handle(mockRequest);
 
-        // Assert
         assertFalse(result);
-        verify(mockRequest, times(1)).filtration();
+        verify(mockRequest, times(2)).filtration();
         verifyNoMoreInteractions(mockRequest.logRecord());
     }
 
     @Test
     void shouldThrowExceptionWhenFiltrationIsEmpty() {
-        // Arrange
         when(mockRequest.filtration()).thenReturn(Optional.empty());
 
-        // Assert
         assertThrows(RuntimeException.class, () -> {
-            // Act
             handler.handle(mockRequest);
         });
         verify(mockRequest, times(1)).filtration();
